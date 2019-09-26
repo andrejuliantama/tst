0b98008446901e3049dabbc1c2c49bb3a53b7395
andrejuliantama <55067771+andrejuliantama@users.noreply.github.com>
2019-09-26 11:23:32 +0700

    Update SSLServer.py

--- SSLServer.py
+++ SSLServer.py
@@ -8,7 +8,7 @@ Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.socket = ssl.wrap_socket(httpd.socket, 
-                                   keyfile = "C:\\Users\\Andre J\\Desktop\\privatekey.key", 
-                                   certfile = "C:\\Users\\Andre J\\Desktop\\certificate.crt", 
+                                   keyfile = "privatekey.key", 
+                                   certfile = "certificate.crt", 
                                    server_side=True)
     httpd.serve_forever()
